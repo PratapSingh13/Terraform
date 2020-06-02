c50663faa33f90d9acc50c19b13e71232304689a
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-06-02 22:47:54 +0530

    Update variables.tf

--- aws | Infra/stagging/variables.tf
+++ aws | Infra/stagging/variables.tf
@@ -1,1 +1,11 @@
-kcm
+variable "accessKey" {
+  default = "********************"
+}
+
+variable "secretKey" {
+  default = "******************************"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
