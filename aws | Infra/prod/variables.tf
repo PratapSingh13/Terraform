9c2c67c3c361d65c61e0c6c7548a6d85e4d0cdab
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-06-02 22:47:19 +0530

    Update variables.tf

--- aws | Infra/prod/variables.tf
+++ aws | Infra/prod/variables.tf
@@ -1,1 +1,11 @@
-cd
+variable "accessKey" {
+  default = "********************"
+}
+
+variable "secretKey" {
+  default = "******************************"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
